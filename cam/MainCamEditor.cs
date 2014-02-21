6fbfada6786a66e1abb74b41d129488a79455d2f
moketao <moketao@qq.com>
2014-02-21 10:27:38 +0800

    v1

---
+++ cam/MainCamEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof(MainCam))]
+public class MainCamEditor : PropertyEditor {
+	private SerializedProperty p0;
+	private SerializedProperty p1;
+	private SerializedProperty p2;
+	protected override void Initialize(){
+		p0 = serializedObject.FindProperty("p0");
+		p1 = serializedObject.FindProperty("p1");
+		p2 = serializedObject.FindProperty("p2");
+	}
+	public override void OnInspectorGUI (){
+		BeginEdit ();
+			BeginSection ("h4");
+				PropertyField ("h5", p0);
+				Comment ("SomeThing to say");
+				MinMaxPropertySliderFields ("h6", p1, p2, 0f, 100f);
+			EndSection ();
+		EndEdit ();
+	}
+}
+
